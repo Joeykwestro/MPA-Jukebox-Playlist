c663fa8d8135d65c83131b0a9dfd54ce3e11b0f2
Joey <99062903@mydavinci.nl>
2022-09-26 18:53:30 +0200

    song class aangemaakt

---
+++ MPA-Jukebox-Playlist/MPA_Jukebox_Playlist/Models/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MPA_Jukebox_Playlist.MPA_Jukebox_Playlist.Models
+{
+    public partial class Song
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int GenreID { get; set; }
+        public int Duration { get; set; }
+    }
+}
